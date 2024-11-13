f089403346ed77c9e96c2b795f5488f6577df5c8
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-13 23:54:17 +0000

    Update configuration

--- sagacity/config/settings.py
+++ sagacity/config/settings.py
@@ -80,12 +80,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
