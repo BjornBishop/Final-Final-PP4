73107a2be3ce09fd80a5735a0c7845c8ee1bd039
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-14 00:15:23 +0000

    Fix Python path for dashboard app

--- manage.py
+++ manage.py
@@ -6,17 +6,18 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Add the parent directory to Python path
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'sagacity'))  # Added this line
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sagacity.config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Are you sure it's installed?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
