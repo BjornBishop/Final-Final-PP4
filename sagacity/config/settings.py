09a5dcbbb53da27625a0a3046bb8eefa1a03ecc3
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-14 03:32:31 +0000

    Update database settings for Heroku

--- sagacity/config/settings.py
+++ sagacity/config/settings.py
@@ -78,10 +78,7 @@ WSGI_APPLICATION = 'sagacity.config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
