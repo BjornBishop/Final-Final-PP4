ddc5abeb914a00d374e4cd95061cdf2a76c25ff0
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-14 00:42:33 +0000

    going around in circles here

--- sagacity/config/settings.py
+++ sagacity/config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4', 
-    'dashboard',  # Change to just 'dashboard'
+    'sagacity.dashboard',  # Change to just 'dashboard'
 ]
 
 MIDDLEWARE = [
