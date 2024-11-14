957b8eaa90f1baec939d7b418131feebbff21167
Bjorn Bishop <bjornbishop@gmail.com>
2024-11-14 02:29:33 +0000

    removed sagacity from template DIRS

--- sagacity/config/settings.py
+++ sagacity/config/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'sagacity.config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'sagacity', 'templates')],  # updated this path
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
